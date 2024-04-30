c47d8bb3d36046607891f956caf4136dc75db0bc
ikhsanpermana2501 <164729972+ikhsanpermana2501@users.noreply.github.com>
2024-04-30 04:20:17 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// toggle class active
+const NavbarNav= Document.querySelector('navbar-nav');
+// ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').onclick = () => 
+navbarnav.classList.toggle('active')  
